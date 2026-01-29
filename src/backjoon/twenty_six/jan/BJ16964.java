35b8813a28982aaf4fc134d4bc4a696159572443
KJBig <101792740+KJBig@users.noreply.github.com>
2026-01-29 14:48:19 +0900

    26.01.29 Gold 2

---
+++ src/backjoon/twenty_six/jan/BJ16964.java
@@ -0,0 +1,86 @@
+package backjoon.twenty_six.jan;
+
+import java.io.*;
+import java.util.*;
+
+public class BJ16964 {
+
+    static List<List<Integer>> GRAPH = new ArrayList<>();
+    static List<Integer> ANSWER = new ArrayList<>();
+    static boolean[] VISITED;
+    static int[] POS;
+    static int[] ORDER;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+
+        for (int i=0; i<=N; i++) {
+            GRAPH.add(new ArrayList<>());
+        }
+
+        for (int i=0; i<N-1; i++) {
+            String[] input = br.readLine().split(" ");
+            int start = Integer.parseInt(input[0]);
+            int end = Integer.parseInt(input[1]);
+
+            GRAPH.get(start).add(end);
+            GRAPH.get(end).add(start);
+        }
+
+        POS = new int[N+1];
+        ORDER = new int[N+1];
+
+        String[] orderInput = br.readLine().split(" ");
+
+        for (int i=1; i<=N; i++) {
+            ORDER[i] = Integer.parseInt(orderInput[i-1]);
+            POS[ORDER[i]] = i;
+        }
+
+        for (int i=1; i<=N; i++) {
+            GRAPH.get(i).sort(Comparator.comparingInt(number -> POS[number]));
+        }
+
+        VISITED = new boolean[N+1];
+
+        dfs(1);
+
+        for (Integer i : ANSWER) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        System.out.println("======");
+        for (Integer i : POS) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println();
+
+        for(int i=0; i<N; i++) {
+            if(ANSWER.get(i) != ORDER[i+1]) {
+                System.out.println(0);
+                return;
+            }
+        }
+
+        System.out.println(1);
+    }
+
+    private static void dfs(int start) {
+        if (VISITED[start]) {
+            return;
+        }
+
+        System.out.println("start: " + start);
+        VISITED[start] = true;
+        ANSWER.add(start);
+
+        for (int i : GRAPH.get(start)) {
+            if (!VISITED[i]) {
+                dfs(i);
+            }
+        }
+    }
+
+}
