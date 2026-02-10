fd2b2d715c6ad5147bb295f762523c118cf4d4fd
KJBig <101792740+KJBig@users.noreply.github.com>
2026-02-10 18:35:41 +0900

    26.02.10 Bronze 1

---
+++ src/backjoon/twenty_six/feb/BJ24264.java
@@ -0,0 +1,13 @@
+package backjoon.twenty_six.feb;
+
+import java.io.*;
+
+public class BJ24264 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        long n = Long.parseLong(br.readLine());
+        System.out.println(n * n);
+        System.out.println(2);
+    }
+}
